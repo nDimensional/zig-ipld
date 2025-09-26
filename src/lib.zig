3bbddfd9858b9d423a2e2bcac3ec1ac15d7fe70b
Joel Gustafson <joelgustafson@protonmail.com>
2025-09-26 14:27:18 -0400

    Update lib.zig

--- src/lib.zig
+++ src/lib.zig
@@ -6,31 +6,3 @@ pub const Value = value.Value;
 
 pub const Bytes = @import("types/Bytes.zig");
 pub const String = @import("types/Bytes.zig");
-
-// pub const Bytes = struct {
-//     data: []const u8,
-
-//     pub fn parseIpldBytes(allocator: std.mem.Allocator, data: []const u8) !Bytes {
-//         const copy = try allocator.alloc(u8, data.len);
-//         @memcpy(copy, data);
-//         return .{ .data = copy };
-//     }
-
-//     pub fn writeIpldBytes(self: Bytes, writer: *std.io.Writer) !void {
-//         try writer.writeAll(self.data);
-//     }
-// };
-
-// pub const String = struct {
-//     data: []const u8,
-
-//     pub fn parseIpldString(allocator: std.mem.Allocator, data: []const u8) !String {
-//         const copy = try allocator.alloc(u8, data.len);
-//         @memcpy(copy, data);
-//         return .{ .data = copy };
-//     }
-
-//     pub fn writeIpldString(self: String, writer: *std.io.Writer) !void {
-//         try writer.writeAll(self.data);
-//     }
-// };
